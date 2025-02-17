e277663bc5adab25103e5aeb915014e4c3a82d9b
Jesse Cardone <jessecar96@gmail.com>
2025-02-16 23:55:20 -0500

    Add comments to install.sh

--- install.sh
+++ install.sh
@@ -6,29 +6,37 @@ if [ "$EUID" -eq 0 ]
 fi
 
 # Update OS
+echo "## Updating OS ##"
 sudo apt-get update
 sudo apt-get upgrade -y
 
 # Install git
+echo "## Installing git ##"
 sudo apt-get install git
 
 # Check if .dotnet folder exists
+echo "## Checking for .NET ##"
 if [ ! -d $HOME/.dotnet ]; then
 
   # Install .NET LTS
+  echo "## Installing .NET ##"
   curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel LTS
 
   # Add to PATH
+  # shellcheck disable=SC2016
   echo 'export DOTNET_ROOT=$HOME/.dotnet' >> $HOME/.bashrc
+  # shellcheck disable=SC2016
   echo 'export PATH=$PATH:$HOME/.dotnet' >> $HOME/.bashrc
-  source $HOME/.bashrc
+  source "$HOME/.bashrc"
 
 fi
 
 # Enable SPI, this requires reboot
+echo "## Enabling SPI ##"
 sudo sed -i '/spi=on/s/^#//g' /boot/firmware/config.txt
 
 # git clone project
+echo "## Downloading project ##"
 cd $HOME
 git clone https://github.com/Jessecar96/wsjr-data-encoder.git $HOME/wsjr-data-encoder
 cd $HOME/wsjr-data-encoder
@@ -40,14 +48,16 @@ git fetch --tags
 latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 
 # Checkout latest tag
-git checkout $latestTag
+git checkout -q $latestTag
 
 # Build project
 # Must be built as debug, release does not work for some reason
+echo "## Building project ##"
 mkdir -p $HOME/jrencoder
 dotnet build --nologo --configuration Debug --property:OutputPath=$HOME/jrencoder/ -property:WarningLevel=0 $HOME/wsjr-data-encoder/JrEncoder.sln
 
 # Create systemd service
+echo "## Installing service ##"
 cat << EOL | sudo tee /etc/systemd/system/jrencoder.service
 [Unit]
 Description=Weather Star Data Encoder
@@ -72,9 +82,10 @@ sudo systemctl daemon-reload
 sudo systemctl enable jrencoder.service
 
 # Generate config file
+echo "## Generating config file ##"
 cd $HOME/jrencoder
 $HOME/.dotnet/dotnet $HOME/jrencoder/JrEncoder.dll --create-config
 
-echo "Install complete!"
+printf "\n\nInstall complete!\n"
 echo "config.json file was created in $HOME/jrencoder/"
 echo "Edit your config.json file using nano/vi/vim then reboot your pi to start the program"
