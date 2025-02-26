4729c51cb6af0bca342f841e2a30d9024b58502c
Jesse Cardone <jessecar96@gmail.com>
2025-02-26 14:10:36 -0500

    Set defaults in TextLineAttributes

--- JrEncoderLib/StarAttributes/TextLineAttributes.cs
+++ JrEncoderLib/StarAttributes/TextLineAttributes.cs
@@ -5,37 +5,41 @@ public struct TextLineAttributes
     /// <summary>
     /// Adds a solid line above the text when it is rendered.
     /// </summary>
-    public bool Separator;
+    public bool Separator = false;
 
     /// <summary>
     /// Makes the text flash.
     /// </summary>
-    public bool Flash;
+    public bool Flash = false;
 
     /// <summary>
     /// Switches the color of the text fill and the text border.
     /// </summary>
-    public bool Reverse;
+    public bool Reverse = false;
 
     /// <summary>
     /// Enables an outline around the text.
     /// </summary>
-    public bool Border;
+    public bool Border = true;
 
     /// <summary>
     /// The color of the text.
     /// </summary>
-    public Color Color;
+    public Color Color = Color.Blue;
 
     /// <summary>
     /// The width of the text. 0 or 1
     /// </summary>
-    public int Width;
+    public int Width = 0;
 
     /// <summary>
     /// The height of the text. 0, 1, or 2
     /// </summary>
-    public int Height;
+    public int Height = 1;
+
+    public TextLineAttributes()
+    {
+    }
 
     /// <summary>
     /// Returns the two-byte packet for this TextLineAttributes.
